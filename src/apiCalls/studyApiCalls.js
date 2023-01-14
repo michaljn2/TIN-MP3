d116c430a97f431b0a8f504193e3a08e4abc4155
Michał Januszewski s22882 <s22882@pjwstk.edu.pl>
2023-01-14 13:10:12 +0100

    Zapytania API dla przynaleznosci

--- src/apiCalls/studyApiCalls.js
+++ src/apiCalls/studyApiCalls.js
@@ -7,3 +7,29 @@ export function getStudyByIdApiCall(studyId){
     const url = `${studiesBaseUrl}/${studyId}`;
     return fetch(url);
 }
+
+export function addStudyApiCall(study){
+    const studyString = JSON.stringify(study);
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: studyString
+    };
+    const promise = fetch(studiesBaseUrl, options);
+    return promise;
+}
+export function updateStudyApiCall(studyId, study) {
+    const studyString = JSON.stringify(study);
+    const url = `${studiesBaseUrl}/${studyId}`;
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: studyString
+    };
+    const promise = fetch(url, options);
+    return promise;
+}
