0d934259467dc8bdd330ef27b2dfa99456a46a3f
Michał Januszewski s22882 <s22882@pjwstk.edu.pl>
2023-01-17 21:26:01 +0100

    Przyciski zmiany jezyka

--- src/components/fragments/Navigation.js
+++ src/components/fragments/Navigation.js
@@ -1,19 +1,35 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {useTranslation} from "react-i18next";
+import {withTranslation} from "react-i18next";
 
-function Navigation() {
-    const { t } = useTranslation();
-    return(
-        <nav>
-            <ul>
-                <li><Link to="/">{t('nav.main-page')}</Link></li>
-                <li><Link to="/students">{t('nav.students')}</Link></li>
-                <li><Link to="/groups">{t('nav.groups')}</Link></li>
-                <li><Link to="/studies">{t('nav.studies')}</Link></li>
-            </ul>
-        </nav>
-    )
+
+class Navigation extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    handleLanguageChange = (language) => {
+        const {i18n} = this.props;
+        i18n.changeLanguage(language, (err, t) => {
+            if (err) return console.log('something went wrong loading', err);
+        });
+    }
+
+    render() {
+        const {t} = this.props;
+        return(
+            <nav>
+                <ul>
+                    <li><Link to="/">{t('nav.main-page')}</Link></li>
+                    <li><Link to="/students">{t('nav.students')}</Link></li>
+                    <li><Link to="/groups">{t('nav.groups')}</Link></li>
+                    <li><Link to="/studies">{t('nav.studies')}</Link></li>
+                    <li className='lang' ><button onClick={() => {this.handleLanguageChange('pl') }}>PL</button> </li>
+                    <li className='lang' ><button onClick={() => {this.handleLanguageChange('en') }}>EN</button> </li>
+                </ul>
+            </nav>
+        )
+    }
 }
 
-export default Navigation
+export default withTranslation() (Navigation)
